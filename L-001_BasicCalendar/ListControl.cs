f5463dc3adc0299132d44f1acad2415d2a881877
Takahiro Nakamura <t_nakamura@qualiapp.biz>
2020-11-17 13:43:05 +0900

    配列の説明用コードです。

---
+++ L-001_BasicCalendar/ListControl.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _countRow = 5;
+            int _countCol = 7;
+            int _countAll = _countRow * _countCol;
+
+            var _listDays = Enumerable.Range(1, _countAll).ToArray();
+            string _drawText = "";
+
+            for (int _countDay = 0; _countDay < _countAll; _countDay++)
+            {
+                int _checkDay = _countDay % _countCol;
+
+                if (_checkDay == 0)
+                {
+                    _drawText += "\n";
+                }
+
+                _drawText += _listDays[_countDay] + " ";
+            }
+
+            Console.WriteLine(_drawText);
+        }
+    }
+}
